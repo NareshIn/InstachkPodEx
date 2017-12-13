0dfb1a12a5a036291bbbfede3a46596be268934a
SandeepKumarPN <sandeepkumarpn1234@gmail.com>
2017-12-13 15:47:13 +0530

    Initial Commit

---
+++ InstachkPodEx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstachkPodEx
+//
+//  Created by Naresh on 13/12/17.
+//  Copyright © 2017 Naresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
